a05ac21f6cf8313b98c973d7c23b08d8267295a2
Kyle Lozinski <kclozins@lakeheadu.ca>
2021-10-18 21:11:09 -0400

    Added Projectile Spritesheet

--- export/neko/haxe/ApplicationMain.hx
+++ export/neko/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "93";
+		app.meta["build"] = "3";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "Project 1";
 		app.meta["name"] = "Project 1";
